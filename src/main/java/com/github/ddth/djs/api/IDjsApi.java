282de224afddeedee5085aaa7fd6559f61881435
Thanh Nguyen <btnguyen2k@gmail.com>
2016-06-03 10:07:02 +0700

    v0.1.3.7

---
+++ src/main/java/com/github/ddth/djs/api/IDjsApi.java
@@ -0,0 +1,47 @@
+package com.github.ddth.djs.api;
+
+import com.github.ddth.djs.message.queue.TaskFinishMessage;
+import com.github.ddth.djs.message.queue.TaskFireoffMessage;
+
+/**
+ * APIs to interact with DJS nodes.
+ * 
+ * @author Thanh Nguyen <btnguyen2k@gmail.com>
+ * @since 0.1.3.7
+ */
+public interface IDjsApi {
+    /**
+     * Polls a job task from server.
+     * 
+     * @param clientId
+     * @return
+     */
+    public TaskFireoffMessage pollTask(String clientId);
+
+    /**
+     * Returns a job task back to server, untouched.
+     * 
+     * @param clientId
+     * @param taskFireoffMessage
+     * @return
+     */
+    public boolean returnTask(String clientId, TaskFireoffMessage taskFireoffMessage);
+
+    /**
+     * Notifies that job task has been picked up by a worker.
+     * 
+     * @param clientId
+     * @param taskFireoffMessage
+     * @return
+     */
+    public boolean notifyTaskPickup(String clientId, TaskFireoffMessage taskFireoffMessage);
+
+    /**
+     * Notifies that worker has finished a job task.
+     * 
+     * @param clientId
+     * @param taskFinishMessage
+     * @return
+     */
+    public boolean notifyTaskFinish(String clientId, TaskFinishMessage taskFinishMessage);
+}
--- src/main/java/com/github/ddth/djs/utils/DjsConstants.java
+++ src/main/java/com/github/ddth/djs/utils/DjsConstants.java
@@ -10,14 +10,20 @@ import java.nio.charset.Charset;
  */
 public class DjsConstants {
 
-	public final static Charset UTF8 = Charset.forName("UTF-8");
+    public final static Charset UTF8 = Charset.forName("UTF-8");
 
-	public final static int TASK_STATUS_NEW = 0;
-	public final static int TASK_STATUS_PICKED = 1;
-	public final static int TASK_STATUS_SKIPPED = 2;
-	public final static int TASK_STATUS_FINISHED_OK = 10;
-	public final static int TASK_STATUS_FINISHED_CANCEL = 11;
-	public final static int TASK_STATUS_FINISHED_ERROR = 12;
-	public final static int TASK_STATUS_DEFAULT = TASK_STATUS_NEW;
+    public final static int TASK_STATUS_NEW = 0;
+    public final static int TASK_STATUS_PICKED = 1;
+    public final static int TASK_STATUS_SKIPPED = 2;
+    public final static int TASK_STATUS_FINISHED_OK = 10;
+    public final static int TASK_STATUS_FINISHED_CANCEL = 11;
+    public final static int TASK_STATUS_FINISHED_ERROR = 12;
+    public final static int TASK_STATUS_DEFAULT = TASK_STATUS_NEW;
+
+    public final static int RESPONSE_OK = 200;
+    public final static int RESPONSE_NOT_FOUND = 404;
+    public final static int RESPONSE_ACCESS_DENIED = 403;
+    public final static int RESPONSE_CLIENT_ERROR = 400;
+    public final static int RESPONSE_SERVER_ERROR = 500;
 
 }
